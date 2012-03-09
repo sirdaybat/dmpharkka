de3a9b052049d507d7f2ca891f8881fcd1ce43eb
Nuutti Hölttä <nuutti.holtta@luukku.com>
2012-03-09 10:29:09 +0200

    Fixed background gap bug; added image loader.

--- code.js
+++ code.js
@@ -30,8 +30,8 @@ var backimg = new Image();
 
 var player = new Thing();
 
-var update_delay = 20;
-var draw_delay = 20;
+var update_delay = 10;
+var draw_delay = 10;
 var downkeys = [];
 
 function keyDown(evt){
@@ -46,7 +46,41 @@ function realtime(){
 	return (new Date()).getTime() - starttime;
 }
 
+var imagepaths = Object.freeze({
+	background: "resources/background-24x24.png",
+	ship: "resources/ship-24x24.png",
+	greenenemy: "resources/greenenemy-24x24.png",
+});
+
+var images = {};
+var imagesLoaded = 0;
+var imageCount = 0;
+
 function init(){
+	loadImages();
+}
+
+var imageWaitInterval;
+
+function loadImages(){
+	for(imagehandle in imagepaths){
+		images[imagehandle] = new Image();
+		images[imagehandle].onload = function(){ imagesLoaded++; };
+		images[imagehandle].src = imagepaths[imagehandle];
+		imageCount++;
+	}
+	
+	imageWaitInterval = window.setInterval("waitForImages()", 100);
+}
+
+function waitForImages(){	
+	if(imagesLoaded == imageCount){
+		window.clearInterval(imageWaitInterval);
+		restOfInit();
+	}
+}
+
+function restOfInit(){
 	canvas = document.getElementById("can");
 	con = canvas.getContext("2d");
 	
@@ -54,21 +88,19 @@ function init(){
 
 	window.addEventListener("keydown", keyDown, true);
 	window.addEventListener("keyup", keyUp, false);
-
+	
 	window.setInterval("update()", update_delay);
 	window.setInterval("draw()", draw_delay);
-	
-	backimg.src = "resources/background-24x24.png";
 }
 
 function update(){
 	while(counter < realtime()){
 		view_bottom += scrollspeed*update_delay;
 		
-		if(downkeys[65]) player.x -= 0.2*update_delay;
-		if(downkeys[68]) player.x += 0.2*update_delay;
-		if(downkeys[87]) player.y += 0.2*update_delay;
-		if(downkeys[83]) player.y -= 0.2*update_delay;
+		if(downkeys[37]) player.x -= 0.2*update_delay;
+		if(downkeys[39]) player.x += 0.2*update_delay;
+		if(downkeys[38]) player.y += 0.2*update_delay;
+		if(downkeys[40]) player.y -= 0.2*update_delay;
 		player.y += scrollspeed*update_delay;
 		
 		counter += update_delay;
@@ -86,21 +118,19 @@ function draw(){
 	var back_end = wrdY(0);
 	for(y = back_start; y <= back_end + 24; y += 24){
 		var back_idx = Math.floor(y / 24);
-		var scry = scrY(back_idx*24+23);
+		var scry = Math.round(scrY(back_idx*24+23));
+		var i;
 		for(i = 0; i < 10; i++){
-			if(background[background.length - 1 - back_idx].charAt(i) == '1') con.drawImage(backimg, i*24, scry);
+			var real_idx = background.length - 1 - back_idx;
+			if(real_idx >= 0){
+				if(background[real_idx].charAt(i) == '1') con.drawImage(images.background, i*24, scry);
+			}
 		}
 	}
 	
-	
 	con.font = "10pt Monospace";
 	con.fillText("real time " + realtime(), 10, 30);
-	con.fillText("counter   " + counter, 10, 60);
-	
-	
-	
-	
-	
+	con.fillText("counter   " + counter, 10, 60);	
 	
 	player.draw();
 
