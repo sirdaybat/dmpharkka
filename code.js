350598f6fcc6f412a969105c1b63308e0494410b
Nuutti Hölttä <nuutti.holtta@luukku.com>
2012-03-09 13:59:25 +0200

    Moved player control to sh.player.update; added temporary test enemy.

--- code.js
+++ code.js
@@ -1,21 +1,13 @@
 "use strict";
 
-if (typeof Object.create !== 'function') {
-    Object.create = function (o) {
-        function F() {}
-        F.prototype = o;
-        return new F();
-    };
-}
-
 var sh = {};
 
 sh.gameObjectTypes = Object.freeze({
     unidentified : 0,
-    playerShip : 0,
-    enemy : 100,
-    playerBullet : 200,
-    enemyBullet : 300
+    playerShip : 100,
+    enemy : 200,
+    playerBullet : 300,
+    enemyBullet : 400
 });
 
 sh.gameObject = {
@@ -23,13 +15,9 @@ sh.gameObject = {
     x : 100,
     y : 100,
     width : 50,
-    height : 50
+    height : 50,
+    update : function(){}
 };
-
-sh.counter = 0;
-
-sh.view_bottom = 0;
-sh.scrollspeed = 320 / 5 / 1000;
 
 sh.scrY = function (world_y) {
 	return sh.canvas.height - 1 - world_y + sh.view_bottom;
@@ -39,26 +27,14 @@ sh.wrdY = function (screen_y) {
 	return sh.canvas.height - 1 - screen_y + sh.view_bottom;
 }
 
-sh.Thing = function () {
-	this.x = 10;
-	this.y = 50;
-	this.w = 24;
-	this.h = 24;
+sh.player = Object.create(sh.gameObject);
+sh.player.update = function(){
+	if(sh.downkeys[37]) this.x -= 0.2*sh.update_delay;
+	if(sh.downkeys[39]) this.x += 0.2*sh.update_delay;
+	if(sh.downkeys[38]) this.y += 0.2*sh.update_delay;
+	if(sh.downkeys[40]) this.y -= 0.2*sh.update_delay;
+	this.y += sh.scrollspeed*sh.update_delay;
 }
-
-sh.Thing.prototype.draw = function () {
-	if(this.image !== undefined) sh.con.drawImage(this.image, this.x, sh.scrY(this.y));
-	else{
-		sh.con.fillStyle = "red";
-		sh.con.fillRect(this.x, sh.scrY(this.y), this.w, this.h);
-	}
-}
-
-sh.player = new sh.Thing();
-
-sh.update_delay = 10;
-sh.draw_delay = 10;
-sh.downkeys = [];
 
 sh.keyDown = function(evt){
 	sh.downkeys[evt.keyCode] = true;
@@ -80,17 +56,15 @@ sh.imagepaths = Object.freeze({
 	whitebullet: "resources/whitebullet-10x10.png"
 });
 
-sh.images = {};
-sh.imagesLoaded = 0;
-sh.imageCount = 0;
-
 sh.init = function(){
+	sh.images = {};
+	sh.imagesLoaded = 0;
+	sh.imageCount = 0;
 	sh.loadImages();
 }
 
 sh.loadImages = function(){
-	var imagehandle;
-	for(imagehandle in sh.imagepaths){
+	for(var imagehandle in sh.imagepaths){
 		sh.images[imagehandle] = new Image();
 		sh.images[imagehandle].onload = function(){ sh.imagesLoaded++; };
 		sh.images[imagehandle].src = sh.imagepaths[imagehandle];
@@ -113,29 +87,42 @@ sh.restOfInit = function(){
 	sh.con = sh.canvas.getContext("2d");
 	
 	sh.starttime = (new Date()).getTime();
+	
+	sh.gametime = 0;
+	sh.view_bottom = 0;
+	sh.scrollspeed = 320 / 5 / 1000;
+	sh.update_delay = 10;
+	sh.draw_delay = 10;
+	sh.downkeys = [];
+	sh.enemies = [];
+	
+	var testenemy = Object.create(sh.gameObject);
+	testenemy.x = 0;
+	testenemy.y = 100;
+	testenemy.type = sh.gameObjectTypes.enemy;
+	testenemy.update = function(){
+		this.x = sh.player.x + Math.cos(sh.gametime*0.004)*50;
+		this.y = sh.player.y + Math.sin(sh.gametime*0.004)*50;
+	}
+	sh.enemies.push(testenemy);
 
 	window.addEventListener("keydown", sh.keyDown, true);
 	window.addEventListener("keyup", sh.keyUp, false);
 	
 	window.setInterval("sh.update()", sh.update_delay);
 	window.setInterval("sh.draw()", sh.draw_delay);
-	
-	sh.player.image = sh.images.ship;
 }
 
 sh.update = function(){
-	while(sh.counter < sh.realtime()){
+	while(sh.gametime < sh.realtime()){
 		sh.view_bottom += sh.scrollspeed*sh.update_delay;
 		
-		if(sh.downkeys[37]) sh.player.x -= 0.2*sh.update_delay;
-		if(sh.downkeys[39]) sh.player.x += 0.2*sh.update_delay;
-		if(sh.downkeys[38]) sh.player.y += 0.2*sh.update_delay;
-		if(sh.downkeys[40]) sh.player.y -= 0.2*sh.update_delay;
-		sh.player.y += sh.scrollspeed*sh.update_delay;
+		sh.player.update();
+		for(var eidx in sh.enemies) sh.enemies[eidx].update();
 		
-		sh.counter += sh.update_delay;
+		sh.gametime += sh.update_delay;
 	}
-	while(sh.counter > sh.realtime());
+	while(sh.gametime > sh.realtime());
 }
 
 sh.draw = function(){
@@ -143,14 +130,12 @@ sh.draw = function(){
 	sh.con.fillRect(0, 0, sh.canvas.width, sh.canvas.height);
 	sh.con.fillStyle = "red";
 	
-	var y;
 	var back_start = sh.wrdY(sh.canvas.height-1);
 	var back_end = sh.wrdY(0);
-	for(y = back_start; y <= back_end + 24; y += 24){
+	for(var y = back_start; y <= back_end + 24; y += 24){
 		var back_idx = Math.floor(y / 24);
 		var screeny = Math.round(sh.scrY(back_idx*24+23));
-		var i;
-		for(i = 0; i < 10; i++){
+		for(var i = 0; i < 10; i++){
 			var real_idx = sh.leveldata.length - 1 - back_idx;
 			if(real_idx >= 0){
 				if(sh.leveldata[real_idx][0].charAt(i) === '1') sh.con.drawImage(sh.images.background, i*24, screeny);
@@ -160,8 +145,12 @@ sh.draw = function(){
 	
 	sh.con.font = "10pt Monospace";
 	sh.con.fillText("real time " + sh.realtime(), 10, 30);
-	sh.con.fillText("counter   " + sh.counter, 10, 60);	
+	sh.con.fillText("gametime   " + sh.gametime, 10, 60);	
 	
-	sh.player.draw();
+	sh.con.drawImage(sh.images.ship, sh.player.x, sh.scrY(sh.player.y));
+	for(var idx in sh.enemies){
+		var enemyship = sh.enemies[idx];
+		sh.con.drawImage(sh.images.greenenemy, enemyship.x, sh.scrY(enemyship.y));
+	}
 
 }
